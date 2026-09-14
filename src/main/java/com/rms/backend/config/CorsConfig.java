c05838461672e690c5b8c3d0ac66234ca8fde9b7
Biju K <bijukrish03@gmail.com>
2026-09-14 16:48:07 +0530

    All enpoints created

--- src/main/java/com/rms/backend/config/CorsConfig.java
+++ src/main/java/com/rms/backend/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","https://rms-frontend-xumo.onrender.com/dashboard")
                 .allowedMethods(
                         "GET",
                         "POST",
